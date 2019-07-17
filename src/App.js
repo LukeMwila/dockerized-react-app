8c992b7efc8b693e50480115d2f30ec66a84ce20
Lukonde Francis Mwila <lukonde.mwila@entelect.co.za>
2019-07-17 21:26:44 +0200

    Changed text in the App.js file

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          I changed this on a feature branch
         </p>
         <a
           className="App-link"
